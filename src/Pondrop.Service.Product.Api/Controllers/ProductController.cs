153c1796194307cee5394cfe96fb66ce82f4839e
Bryan <Bryan@xam.com.au>
2022-11-04 08:11:17 +0000

    Merged PR 230: Added create product with categories endpoint
    Related work items: #910

--- src/Pondrop.Service.Product.Api/Controllers/ProductController.cs
+++ src/Pondrop.Service.Product.Api/Controllers/ProductController.cs
@@ -97,6 +97,48 @@ public class ProductController : ControllerBase
     }
 
     [HttpPost]
+    [Route("createwithcategories")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateProductWithCategories([FromBody] CreateProductWithCategoriesCommand command)
+    {
+        var result = await _mediator.Send(new CreateProductCommand()
+        {
+            Name = command.Name,
+            AltName = command.AltName,
+            ShortDescription = command.ShortDescription,
+            BrandId = command.BrandId,
+            NetContent = command.NetContent,
+            NetContentUom = command.NetContentUom,
+            ChildProductId = command.ChildProductId,
+            Variant = command.Variant,
+            PossibleCategories = command.PossibleCategories,
+            PublicationLifecycleId = command.PublicationLifecycleId,
+            ExternalReferenceId = command.ExternalReferenceId,
+        });
+
+        return await result.MatchAsync<IActionResult>(
+            async i =>
+            {
+                await _serviceBusService.SendMessageAsync(new UpdateProductCheckpointByIdCommand() { Id = i!.Id });
+                var productCategoryResult = await _mediator.Send(new SetProductCategoriesCommand() { ProductId = i!.Id, CategoryIds = command!.CategoryIds, PublicationLifecycleId = command.PublicationLifecycleId });
+                await productCategoryResult.MatchAsync<IActionResult>(
+                    async productCategories =>
+                    {
+                        foreach (var productCategory in productCategories)
+                        {
+                            await _serviceBusService.SendMessageAsync(new UpdateProductCategoryCheckpointByIdCommand() { Id = productCategory!.Id, ProductId = productCategory!.ProductId, CategoryId = productCategory!.CategoryId });
+                        }
+                        return StatusCode(StatusCodes.Status201Created, i);
+                    }, (ex, msg) => Task.FromResult<IActionResult>(new BadRequestObjectResult(msg)));
+
+                return StatusCode(StatusCodes.Status201Created, i);
+            }, (ex, msg) => Task.FromResult<IActionResult>(new BadRequestObjectResult(msg)));
+    }
+
+
+    [HttpPost]
     [Route("update")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
---
+++ src/Pondrop.Service.Product.Application/Commands/Product/CreateProduct/CreateProductWithCategoriesCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Pondrop.Service.Product.Application.Models;
+using Pondrop.Service.Product.Domain.Models;
+using Pondrop.Service.Product.Domain.Models.Product;
+
+namespace Pondrop.Service.Product.Application.Commands;
+
+public class CreateProductWithCategoriesCommand : IRequest<Result<ProductRecord>>
+{
+    public string Name { get; init; } = string.Empty;
+
+    public Guid? BrandId { get; init; } = null;
+
+    public string ExternalReferenceId { get; init; } = string.Empty;
+
+    public string Variant { get; init; } = string.Empty;
+
+    public string AltName { get; init; } = string.Empty;
+
+    public string ShortDescription { get; init; } = string.Empty;
+
+    public double NetContent { get; init; } = 0;
+
+    public string NetContentUom { get; init; } = string.Empty;
+
+    public string PossibleCategories { get; init; } = string.Empty;
+
+    public List<Guid>? ChildProductId { get; init; } = null;
+
+    public string PublicationLifecycleId { get; init; } = string.Empty;
+
+    public List<Guid>? CategoryIds { get; init; } = null;
+}
+
